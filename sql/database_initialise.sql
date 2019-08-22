223b69557a5aa04baa80192e2f5beeaa51867421
Janes Thomas <janes.thomas@namics.com>
2019-08-22 13:54:13 +0200

    added initial sql script for db and default drivers sql

---
+++ sql/database_initialise.sql
@@ -0,0 +1,15 @@
+create database fone;
+use fone;
+
+create table drivers(
+  driver_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY(driver_id)
+);
+
+create table countries(
+    country_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    tag VARCHAR(10) NOT NULL,
+    PRIMARY KEY(country_id)
+);
